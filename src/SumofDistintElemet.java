a3143aa9975979d82f1e8e10d523f07e86186cda
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-06-15 19:11:18 +0530

    Initial commit

---
+++ src/SumofDistintElemet.java
@@ -0,0 +1,32 @@
+
+public class SumofDistintElemet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] num = { 5, 1, 8, 4, 9, 1, 5 };
+		System.out.println(DistinctElemet(num));
+	}
+
+	public static int DistinctElemet(int[] result) {
+//		result = 0;
+//		return result;
+//		int arr[] = new int[result] 
+		int num = 0;
+		boolean dis;
+		for (int i = 0; i < result.length; i++) {
+//			System.out.println(result[i]);
+			dis = true;
+			for (int j = 0; j < i; j++) {
+				if (result[j] == result[i]) {
+					dis = false;
+					break;
+				}
+			}
+			if (dis) {
+				num = num + result[i];
+			}
+		}
+		return num;
+	}
+
+}
