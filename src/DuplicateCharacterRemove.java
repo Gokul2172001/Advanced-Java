c579b31816d09f89a08b99ba133b06afe39af73a
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-06-17 20:53:12 +0530

    Create DuplicateCharacterRemove.java

---
+++ src/DuplicateCharacterRemove.java
@@ -0,0 +1,13 @@
+
+public class DuplicateCharacterRemove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Character";
+		Duplicate(str);
+	}
+
+	static void Duplicate(String str) {
+
+	}
+}
