611dcb2030cdf7552fe5ef64b50bf5c117ad678b
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2024-06-26 23:02:58 +0530

    Update DuplicateCharacterRemove.java

--- src/DuplicateCharacterRemove.java
+++ src/DuplicateCharacterRemove.java
@@ -2,11 +2,9 @@
 public class DuplicateCharacterRemove {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String str = "Character";
 		Duplicate(str);
 	}
-
 	static void Duplicate(String str) {
 		char[] arr = str.toCharArray();
 		for (int i = 0; i < arr.length; i++) {
