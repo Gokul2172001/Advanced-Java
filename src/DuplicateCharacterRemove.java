bdd5a838c97b8b6014319a5bb354e398c4cede2d
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2024-07-08 23:49:44 +0530

    Update DuplicateCharacterRemove.java

--- src/DuplicateCharacterRemove.java
+++ src/DuplicateCharacterRemove.java
@@ -5,6 +5,7 @@ public class DuplicateCharacterRemove {
 		String str = "Character";
 		Duplicate(str);
 	}
+	
 	static void Duplicate(String str) {
 		char[] arr = str.toCharArray();
 		for (int i = 0; i < arr.length; i++) {
@@ -14,3 +15,4 @@ public class DuplicateCharacterRemove {
 		}
 	}
 }
+
