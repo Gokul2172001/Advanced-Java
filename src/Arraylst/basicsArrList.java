4cb0bc36b9b2decc7d4219f26fa87f42fceb5e0f
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-09-26 23:27:21 +0530

    Create basicsArrList.java

---
+++ src/Arraylst/basicsArrList.java
@@ -0,0 +1,20 @@
+package Arraylst;
+
+import java.util.ArrayList;
+
+public class basicsArrList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Andrews");
+		list.add("Andrews");
+		list.add("Andrews");
+		list.add("Andrews");
+//		System.out.println(String.valueOf(0));
+		System.out.println(list.get(0));
+		System.out.println(list.equals("Andrews"));
+	}
+
+}
