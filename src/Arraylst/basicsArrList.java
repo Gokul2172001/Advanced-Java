9a4c46e8c97fb0c16e7dcef50c8c7ce5ddf339ed
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-09-27 22:26:47 +0530

    Update basicsArrList.java

--- src/Arraylst/basicsArrList.java
+++ src/Arraylst/basicsArrList.java
@@ -9,12 +9,21 @@ public class basicsArrList {
 
 		ArrayList<String> list = new ArrayList<String>();
 		list.add("Andrews");
-		list.add("Andrews");
-		list.add("Andrews");
-		list.add("Andrews");
+		list.add("Mark");
+		list.add("Gensles");
+		list.add("Drelogo");
 //		System.out.println(String.valueOf(0));
-		System.out.println(list.get(0));
+		System.out.println(list);
 		System.out.println(list.equals("Andrews"));
+		System.out.println(list.remove(2));
+		System.out.println(list);
+		list.add(2, "Gensles");
+		System.out.println(list);
+		int size = list.lastIndexOf("Drelogo");
+		System.out.println(size);
+		list.removeAll(list);
+		System.out.println(list);
+		System.out.println("!%^&*");
 	}
 
 }
