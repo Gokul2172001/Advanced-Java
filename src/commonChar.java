cbb8755167630574b9880259aad80bd7aa629bc3
Gokul-ADS <76160646+Gokul2172001@users.noreply.github.com>
2025-07-13 23:37:42 +0530

    checking...!

---
+++ src/commonChar.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class commonChar {
+
+	static ArrayList arr = new ArrayList<String>();
+
+//	System.out.println(arr);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str[] = { "cook", "cake", "peacock" };
+		char[] init = str[0].toCharArray();
+		for (int i = 1; i < str.length; i++) {
+
+		}
+		System.out.println(arr);
+
+	}
+
+}
