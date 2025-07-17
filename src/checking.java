bca17f3034f3e900e2f3ff87ed66f8e407b9839f
Gokul-ADS <76160646+Gokul2172001@users.noreply.github.com>
2025-07-17 22:33:53 +0530

    Create checking.java

---
+++ src/checking.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class checking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] cha1 = new char[0];
+		System.out.println(Arrays.toString(cha1));
+
+		cha1 = new char[2];
+		System.out.println(Arrays.toString(cha1));
+	}
+
+}
